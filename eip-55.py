2286f0010ac8c4c1cb257f14bb3dd933a36dff22
quiquee <enrique.melero@gmail.com>
2022-03-08 09:02:02 +0100

    Added eip-55 validation function for ethereum addresses Todo: this can work only after checking basic 20bytes hex input Todo: Top level function to redirect to different address checks Todo: Plei-signature for whitelisting and validation

---
+++ eip-55.py
@@ -0,0 +1,40 @@
+# This is taken from https://eips.ethereum.org/EIPS/eip-55
+# Author Vitalin Butterin
+
+import eth_utils
+
+def checksum_encode(addr): # Takes a 20-byte binary address as input
+    hex_addr = addr.hex()
+    checksummed_buffer = ""
+
+    # Treat the hex address as ascii/utf-8 for keccak256 hashing
+    hashed_address = eth_utils.keccak(text=hex_addr).hex()
+
+    # Iterate over each character in the hex address
+    for nibble_index, character in enumerate(hex_addr):
+
+        if character in "0123456789":
+            # We can't upper-case the decimal digits
+            checksummed_buffer += character
+        elif character in "abcdef":
+            # Check if the corresponding hex digit (nibble) in the hash is 8 or higher
+            hashed_address_nibble = int(hashed_address[nibble_index], 16)
+            if hashed_address_nibble > 7:
+                checksummed_buffer += character.upper()
+            else:
+                checksummed_buffer += character
+        else:
+            raise eth_utils.ValidationError(
+                f"Unrecognized hex character {character!r} at position {nibble_index}"
+            )
+
+    return "0x" + checksummed_buffer
+
+
+def test(addr_str):
+    addr_bytes = eth_utils.to_bytes(hexstr=addr_str)
+    checksum_encoded = checksum_encode(addr_bytes)
+    assert checksum_encoded == addr_str, f"{checksum_encoded} != expected {addr_str}"
+
+test("0xA123BC123")
+
